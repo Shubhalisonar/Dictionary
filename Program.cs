f24bc70f5c7dfb90bd8c92c6e349691b3ba75111
Shubhalisonar <shubhali97@gmail.com>
2022-06-14 11:35:29 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Dictionary<int,string>countries=new Dictionary<int, string>();
+            countries.Add(91, "India");
+            countries.Add(1, "USA");
+            countries.Remove(1);
+            foreach(var item in countries)
+            {
+                Console.WriteLine(item.Key+" "+item.Value);
+            }
+            Console.ReadLine();
+        }
+    }
+}
